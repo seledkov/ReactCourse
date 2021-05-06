96aec6a0ab92e20c9098763a8f45a4a44737f2a8
unknown <seledkovti@gmail.com>
2021-05-06 05:23:07 +0300

    useReducer end

--- src/_example/Login.tsx
+++ src/_example/Login.tsx
@@ -1,39 +1,50 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState, useReducer, ChangeEvent } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+type Reducer<S, A> = (state: S, action: A) => any;
+type State = { value: string; isValid: boolean };
+type Action = {
+  type: 'USER_INPUT' | 'INPUT_BLUR';
+  value?: string;
+  isValid?: boolean | null;
+};
 
-const emailReducer = (state: any, action: any) => {
+const emailReducer: Reducer<State, Action> = (state, action) => {
   if (action.type === 'USER_INPUT') {
     return {
       value: action.value,
-      isValid: action.value.includes('@'),
+      isValid: action.value!.includes('@'),
     };
   }
-  if (action.type === 'USER_BLUR') {
+  if (action.type === 'INPUT_BLUR') {
     return {
       value: state.value,
       isValid: state.value.includes('@'),
     };
   }
-  console.log(state, action);
+  // console.log(state, action);
+  return { value: '', isValid: false };
+  // throw new Error('unknow action type');
 };
 
-const passReducer = (state: any, action: any) => {
+const passReducer: Reducer<State, Action> = (state: State, action: Action) => {
   console.log(state, action);
   if (action.type === 'USER_INPUT') {
     return {
       value: action.value,
-      isValid: action.value.length > 6,
+      isValid: action.value!.length > 6,
     };
   }
-  if (action.type === 'USER_BLUR') {
+  if (action.type === 'INPUT_BLUR') {
     return {
       value: state.value,
-      isValid: action.value.length > 6,
+      isValid: state.value.length > 6,
     };
   }
+  return { value: '', isValid: false };
+  // throw new Error('unknow action type');
 };
 
 const Login = (props: any) => {
@@ -41,39 +52,39 @@ const Login = (props: any) => {
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: '',
-    isValid: false,
+    isValid: null,
   });
 
   const [passState, dispatchPass] = useReducer(passReducer, {
     value: '',
-    isValid: false,
+    isValid: null,
   });
 
   const emailChangeHandler = (event: any) => {
-    dispatchEmail({ type: 'USER_INPUT', value: event.target.value });
-
-    setFormIsValid(
-      event.target.value.includes('@') && passState?.value.trim().length > 6,
-    );
+    dispatchEmail({
+      type: 'USER_INPUT',
+      value: event.target.value,
+    });
+    setFormIsValid(emailState.isValid && passState.isValid);
   };
 
   const passwordChangeHandler = (event: any) => {
     dispatchPass({ type: 'USER_INPUT', value: event.target.value });
-    console.log(event.target.value);
-    setFormIsValid(event.target.value.trim().length > 6 && emailState?.isValid);
+    setFormIsValid(passState.isValid && emailState.isValid);
+    console.log('formIsValid: ', formIsValid);
   };
 
   const validateEmailHandler = () => {
-    dispatchEmail({ type: 'INPUT_BLUR', value: 'lorem' });
+    dispatchEmail({ type: 'INPUT_BLUR' });
   };
 
   const validatePasswordHandler = () => {
-    dispatchPass({ type: 'INPUT_BLUR', value: 'lorem' });
+    dispatchPass({ type: 'INPUT_BLUR' });
   };
 
   const submitHandler = (event: any) => {
     event.preventDefault();
-    props.onLogin(emailState!.value, passState!.value);
+    props.onLogin(emailState.value, passState.value);
   };
 
   return (
@@ -81,26 +92,26 @@ const Login = (props: any) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailState?.isValid === false ? classes.invalid : ''
+            emailState && emailState.isValid === false ? classes.invalid : ''
           }`}>
           <label htmlFor='email'>E-Mail</label>
           <input
             type='email'
             id='email'
-            value={emailState?.value || ''}
+            value={emailState && emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passState?.isValid === false ? classes.invalid : ''
+            passState && passState.isValid === false ? classes.invalid : ''
           }`}>
           <label htmlFor='password'>Password</label>
           <input
             type='password'
             id='password'
-            value={passState?.value || ''}
+            value={passState && passState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
